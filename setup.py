b8ffdcb44fef4ae35e7ea8e32da19884f8d423ce
Paul Durivage <pauldurivage@gmail.com>
2014-09-02 15:00:33 -0500

    Initial commit of project skel

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='downstream-farmer',
+    version='',
+    packages=['downstream-farmer'],
+    url='',
+    license='',
+    author='Storj Labs',
+    author_email='info@storj.io',
+    description=''
+)
