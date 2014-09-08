d4f07ed1aec753367508ec4d147f751106374e83
Paul Durivage <pauldurivage@gmail.com>
2014-09-07 22:50:09 -0500

    Start building out CLI

--- downstream-farmer/shell.py
+++ downstream-farmer/shell.py
@@ -1,3 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
+
+from .client import DownstreamClient
+
+
+def eval_args(args):
+    dsc = DownstreamClient()
+    if args.gen_challenge:
+         dsc.get_challenges()
+         dsc.answer_challenge()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--answer-challenge', action='store_true',
+                        help='')
+
+
+def main():
+    args = parse_args()
+    eval_args(args)
+
+if __name__ == '__main__':
+    main()
