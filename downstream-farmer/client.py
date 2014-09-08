0a76ba8de27fae0a4a01abc7af1b8fe5171a6c20
Paul Durivage <pauldurivage@gmail.com>
2014-09-07 22:49:49 -0500

    Raise NotImplementedError on meths not complete

--- downstream-farmer/client.py
+++ downstream-farmer/client.py
@@ -12,16 +12,16 @@ class DownstreamClient(object):
         self.server = server_url.strip('/')
 
     def connect(self, url):
-        pass
+        raise NotImplementedError
 
     def store_path(self, path):
-        pass
+        raise NotImplementedError
 
     def get_chunk(self, hash):
-        pass
+        raise NotImplementedError
 
     def challenge(self, hash, challenge):
-        pass
+        raise NotImplementedError
 
     def answer(self, hash, hash_answer):
         pass
@@ -30,3 +30,7 @@ class DownstreamClient(object):
         enc_fname = urlify(os.path.split(filename)[1])
         url = '%s/api/downstream/challenge/%s' % (self.server, enc_fname)
         resp = requests.get(url)
+
+    def answer_challenge(self, filename):
+        enc_fname = self._enc_fname(filename)
+        raise NotImplementedError
