75d97d15351bf95a95753dc51713c0e2acfdccbf
Paul Durivage <pauldurivage@gmail.com>
2014-09-07 22:40:16 -0500

    Add client skel meths

--- downstream-farmer/client.py
+++ downstream-farmer/client.py
@@ -11,6 +11,21 @@ class DownstreamClient(object):
     def __init__(self, server_url):
         self.server = server_url.strip('/')
 
+    def connect(self, url):
+        pass
+
+    def store_path(self, path):
+        pass
+
+    def get_chunk(self, hash):
+        pass
+
+    def challenge(self, hash, challenge):
+        pass
+
+    def answer(self, hash, hash_answer):
+        pass
+
     def get_challenges(self, filename):
         enc_fname = urlify(os.path.split(filename)[1])
         url = '%s/api/downstream/challenge/%s' % (self.server, enc_fname)
