09b707e7c190357cd41953aa4304a331eb7182f5
Paul Durivage <pauldurivage@gmail.com>
2014-09-07 22:14:41 -0500

    Add heartbeat to install reqs

--- setup.py
+++ setup.py
@@ -8,5 +8,11 @@ setup(
     license='',
     author='Storj Labs',
     author_email='info@storj.io',
-    description=''
+    description='',
+    install_requires=[
+        'heartbeat==0.1.2'
+    ],
+    dependency_links = [
+        'https://github.com/Storj/heartbeat/archive/v0.1.2.tar.gz#egg=heartbeat-0.1.2'
+    ]
 )
