688bec4dc00dd1040901ca446c6b6cc7fa6fbbcb
Paul Durivage <pauldurivage@gmail.com>
2014-09-07 22:33:24 -0500

    Add documentation and py3k compat

--- downstream-farmer/utils.py
+++ downstream-farmer/utils.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+
 try:
-    from urllib import urlencode
+    from urllib import quote_plus
 except ImportError:
-    from urllib.parse import urlencode
+    from urllib.parse import quote_plus
 
 
-def urlencode(string):
-    return urlencode(string)
+def urlify(string):
+    """ You might be wondering: why is this here at all, since it's basically
+    doing exactly what the quote_plus function in urllib does. Well, to keep
+    the 2 & 3 stuff all in one place, meaning rather than try to import the
+    urllib stuff twice in each file where url-safe strings are needed, we keep
+    it all in one file: here.
+
+    Supporting multiple Pythons is hard.
+
+    :param string: String to URLify
+    :return: URLified string
+    """
+    return quote_plus(string)
