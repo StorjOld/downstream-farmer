5e3525927546eff5cdc4c85213a1f7f283d967ad
Paul Durivage <pauldurivage@gmail.com>
2014-09-08 22:20:47 -0500

    Add node argument

--- downstream-farmer/shell.py
+++ downstream-farmer/shell.py
@@ -4,19 +4,38 @@
 import argparse
 
 from .client import DownstreamClient
+from .exc import ConnectError
+
+try:
+    from urllib2 import urlopen, URLError
+except ImportError:
+    from urllib.request import urlopen
+    from urllib.error import URLError
+
+
+def check_connectivity(url):
+        """ Check to see if we even get a connection to the server.
+        https://stackoverflow.com/questions/3764291/checking-network-connection
+        """
+        try:
+            urlopen(url, timeout=2)
+        except URLError:
+            raise ConnectError("Could not connect to server.")
 
 
 def eval_args(args):
+    try:
+        check_connectivity()
     dsc = DownstreamClient()
-    if args.gen_challenge:
+    if args.:
          dsc.get_challenges()
          dsc.answer_challenge()
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--answer-challenge', action='store_true',
-                        help='')
+    parser.add_argument('node')
+    parser.add_argument('--answer-challenge', action='store_true')
 
 
 def main():
